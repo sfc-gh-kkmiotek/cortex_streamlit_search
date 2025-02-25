5a2c75ba8ec7a32b853c83a4f2f4da6a982aa906
Kuba Kmiotek <kuba.kmiotek@snowflake.com>
2025-02-25 14:56:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,9 @@ def get_service(name):
     .schemas["public"]\
     .cortex_search_services[name]
 
-search_service_popular = get_service("search_service_popular_2")
-search_service_unpopular = get_service("search_service_un_popular_2")
+version = st.secrets['search']['version']
+search_service_popular = get_service(f"search_service_popular_{version}")
+search_service_unpopular = get_service(f"search_service_un_popular_{version}")
 
 def serialize_batch(batch):
     for col, result in zip(st.columns(len(batch)), batch):
@@ -37,25 +38,26 @@ def serialize(result):
     with st.container():
         st.subheader(result['title'])
         st.image(image_url)
-        col1, col2 = st.columns(2)
+        col1, col2, col3 = st.columns(3)
         with col1:
             st.text(f"views 👀: {result['unique_views']}")
         with col2: 
             st.text(f"app_id 🆔: {result['app_id']}")
-    
+        with col3:
+            st.text(f"owner 👑: {result['owner']}")
 
 def search(query, filters=None, order_by="default"):
     resp_popular = search_service_popular.search(
         query=query,
         filter=filters,
-        columns=["app_id", "title", "unique_views"],
+        columns=["app_id", "title", "unique_views", "owner"],
         limit=20
     )
 
     resp_unpopular = search_service_unpopular.search(
         query=query,
         filter=filters,
-        columns=["app_id", "title", "unique_views"],
+        columns=["app_id", "title", "unique_views", "owner"],
         limit=10
     )
     
@@ -67,6 +69,9 @@ def search(query, filters=None, order_by="default"):
     if order_by == 'unique_views':
         return sorted(resp_popular.results + resp_unpopular.results, key=lambda x: -int(x['unique_views']))
         
+def deduplicate(results):
+    app_ids = set()
+    return [x for x in results if x['app_id'] not in app_ids and not app_ids.add(x['app_id'])]
 
 
 def batch(iterable, batch_size=3):
@@ -78,12 +83,14 @@ def batch(iterable, batch_size=3):
 st.title("Cortex based search engine")
 query = st.text_input("Search", "st.chat")
 with st.expander("Advanced options", expanded=True):
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4 = st.columns(4)
     with col1: 
         components = st.multiselect("Streamlit components", ["table", 'chat_message'], None)
     with col2:
         dependencies = st.multiselect("Python dependencies", ['sklearn', 'openai', 'pandas'], None)
     with col3:
+        owner = st.text_input("Owner", None)
+    with col4:
         order_by = st.selectbox("Order by", ["default", "unique_views", 'relevancy'], 1)
         
     filters = None
@@ -92,10 +99,14 @@ with st.expander("Advanced options", expanded=True):
         and_filters.append({"@and": [{"@contains": {"DEPENDENCIES": c}} for c in dependencies]})
     if components:
         and_filters.append({"@and": [{"@contains": {"COMPONENTS": c}} for c in components]})
+    if owner:
+        and_filters.append({"@eq": {"OWNER": owner}})
     if and_filters:
         filters = {"@and":and_filters}
         
 
 results = search(query, filters, order_by)
+results = deduplicate(results)
+
 for b in batch(results):
     serialize_batch(b)
